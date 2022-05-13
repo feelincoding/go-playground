662964419d0fbb02a2d6692577573f297040b659
feelincoding <feelincoding@gmail.com>
2022-05-13 11:15:38 +0900

    go 표준입출력 추가

---
+++ Go_개념정리/04_go_표준입출력/code/04_ex_01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	var b float64 = 123454321.6789
+	fmt.Print("a:", a, "b:", b)
+	fmt.Println("a:", a, "b:", b)
+	fmt.Printf("a:%d b:%f\n", a, b)
+}
---
+++ Go_개념정리/04_go_표준입출력/code/go.mod
@@ -0,0 +1,3 @@
+module goprojects/04_ex_03
+
+go 1.18
