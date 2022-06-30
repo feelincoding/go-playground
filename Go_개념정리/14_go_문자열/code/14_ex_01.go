7bca471dbcd8cf22ef3002828f1b69b529964c46
feelincoding <feelincoding@gmail.com>
2022-06-30 17:54:55 +0900

    feat: go, 문자열

---
+++ Go_개념정리/14_go_문자열/code/14_ex_01.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "Hello 월드"
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("타입: %T 값: %d 문자값: %c\n", str[i], str[i], str[i])
+	}
+	// str2 := "안녕하세요"
+	// fmt.Printf("%c\n", str2[6])
+	arr := []rune(str)
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("타입: %T 값: %d 문자값: %c\n", arr[i], arr[i], arr[i])
+	}
+}
