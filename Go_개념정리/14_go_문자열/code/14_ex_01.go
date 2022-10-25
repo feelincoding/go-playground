cfaebda1588659b84ea8983c54dc053e69abd392
feelincoding <feelincoding@gmail.com>
2022-10-25 20:06:00 +0900

    feat:golang 다시 시작

--- Go_개념정리/14_go_문자열/code/14_ex_01.go
+++ Go_개념정리/14_go_문자열/code/14_ex_01.go
@@ -11,7 +11,13 @@ func main() {
 	}
 	// str2 := "안녕하세요"
 	// fmt.Printf("%c\n", str2[6])
+	fmt.Print("=====")
+	fmt.Print(len(str))
+	fmt.Print("=====")
 	arr := []rune(str)
+	fmt.Print("=====")
+	fmt.Print(len(arr))
+	fmt.Print("=====")
 	for i := 0; i < len(arr); i++ {
 		fmt.Printf("타입: %T 값: %d 문자값: %c\n", arr[i], arr[i], arr[i])
 	}
